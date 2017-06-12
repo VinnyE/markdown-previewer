17d5caec3d63e2defe2151b6f53438e6ca7cf68f
Vinny <espinalv1@gmail.com>
2017-06-12 05:12:24 -0400

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const TextArea = props => {
+
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TextArea />
+      </div>
+    );
+  }
+}
+
+export default App;
